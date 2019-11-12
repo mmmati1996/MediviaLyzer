52e7f1c1c12313bacf497391a025a61fcbf29b04
Mateusz Sobieszczyk <mmmati1996@live.com>
2019-11-12 09:52:43 +0100

    Added close buttons / listview switching tabs

---
+++ MediviaLyzer/MainWindowModelView.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace MediviaLyzer
+{
+    class MainWindowModelView
+    {
+        public DelegateCommand CloseWindow { get; set; }
+        public DelegateCommand SwitchHomeTab { get; set; }
+        
+
+        public MainWindowModelView()
+        {
+            this.CloseWindow = new DelegateCommand(ClosingWindow);
+            this.SwitchHomeTab = new DelegateCommand(HomeTab);
+        }
+        private void ClosingWindow()
+        {
+            System.Windows.Application.Current.MainWindow.Close();
+        }
+
+        private void HomeTab()
+        {
+            Debug.WriteLine("home");
+        }
+    }
+}
