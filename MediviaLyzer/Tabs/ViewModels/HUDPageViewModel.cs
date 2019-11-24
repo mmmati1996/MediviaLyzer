76f9a9b7f113c3b8241e856b6c3c95726f83b0da
Mateusz Sobieszczyk <mmmati1996@live.com>
2019-11-24 18:44:31 +0100

    small fix

--- MediviaLyzer/Tabs/ViewModels/HUDPageViewModel.cs
+++ MediviaLyzer/Tabs/ViewModels/HUDPageViewModel.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MediviaLyzer.Tabs.ViewModels
 {
-    class HUDPageViewModel
+    class HUDPageViewModel : INotifyPropertyChanged
     {
+        private bool IsBedmakersHUDRunning = false;
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public HUDPageViewModel()
+        {
+        }
+
+        public bool BedmakersHUDStatus
+        {
+            get { return IsBedmakersHUDRunning; }
+            set
+            {
+                IsBedmakersHUDRunning = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("BedmakersHUDStatus"));
+            }
+        }
     }
 }
